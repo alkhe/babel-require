6e76aa4cc8958ff4d04a97bddae5d070f0e86d73
jacobmadsen <jacob@webnuts.com>
2015-08-19 09:00:26 +0200

    minor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import path from 'path'
-var babel = require('babel')
+import babel from 'babel'
 
 // https://github.com/joyent/node/blob/master/lib/module.js
 // http://stackoverflow.com/questions/17581830/load-node-js-module-from-string-in-memory
